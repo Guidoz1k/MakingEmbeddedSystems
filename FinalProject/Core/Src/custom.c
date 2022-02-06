06aaf404f4c934d9287fe8d5bf1b98ee9949acdd
unknown <rodrigo_daltoe@hotmail.com>
2022-02-06 02:38:18 -0300

    md

--- FinalProject/Core/Src/custom.c
+++ FinalProject/Core/Src/custom.c
@@ -21,8 +21,10 @@
 
 #define	BUT_UP	GPIOE,GPIO_PIN_2	// PE02 = UP button
 #define	BUT_DWN	GPIOE,GPIO_PIN_3	// PE03 = DOWN button
-#define	LED_RGT	GPIOE,GPIO_PIN_4	// PE04 = RIGHT button
-#define	LED_LFT	GPIOE,GPIO_PIN_5	// PE05 = LEFT button
+#define	BUT_RGT	GPIOE,GPIO_PIN_4	// PE04 = RIGHT button
+#define	BUT_LFT	GPIOE,GPIO_PIN_5	// PE05 = LEFT button
+
+static uint32_t buttons = 0;
 
 //////////////////////////////////////////////////////////////////////// LCD "ll" functions
 
@@ -134,7 +136,51 @@ void lcd_init(void){
     lcd_pos(0, 0);
 }
 
-////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////// LCD "hal" functions
+
+void lcd_number(uint32_t number, uint32_t size, uint32_t line, uint32_t pos){
+	uint32_t character = 0;
+	uint32_t ten = 0;
+	uint32_t i, j;
+
+/*
+instead of doing
+		size--;
+	and having a signed i, with a for like
+		for(i = size; i >= 0; i--)
+	with a negative threshold and a int32_t i i'll do a
+		number *= 10
+	and a for like
+		for(i = size; i > 0; i--)
+	with a uint32_t i
+*/
+
+	number *= 10;
+	lcd_pos(line, pos);
+	for(i = size; i > 0; i--){
+		ten = 1;
+
+		for(j = 1; j <= i; j++)
+			ten *= 10;
+
+		if(i < size)
+			character = ((number / ten) % 10) + 48;
+		else
+			character = (number / ten) + 48;
+
+		lcd_write(character);
+	}
+}
+
+void lcd_string(const char *pointer, uint32_t line, uint32_t pos){
+	uint32_t counter = 0;
+
+	lcd_pos(line, pos);
+	while((counter++ < 16) && (*pointer != '\0'))
+		lcd_write(*(pointer++));
+}
+
+//////////////////////////////////////////////////////////////////////// main functions
 
 void customSetup(void){
 	//uint8_t aux = 0;
@@ -142,37 +188,41 @@ void customSetup(void){
 	lcd_init();
 	HAL_Delay(500);
 
-	lcd_write('1');
-	lcd_write('5');
-	lcd_write(' ');
-	lcd_write('h');
-	lcd_write('o');
-	lcd_write('r');
-	lcd_write('a');
-	lcd_write('s');
-	lcd_write(' ');
-	lcd_write('d');
-	lcd_write('e');
-	lcd_pos(1, 0);
-	lcd_write('t');
-	lcd_write('r');
-	lcd_write('a');
-	lcd_write('m');
-	lcd_write('p');
-	lcd_write('o');
-	lcd_write(' ');
-	lcd_write('d');
-	lcd_write('e');
-	lcd_write('p');
-	lcd_write('o');
-	lcd_write('i');
-	lcd_write('s');
-	lcd_write(' ');
-	lcd_write(' ');
-	lcd_write(' ');
 }
 
 void customLoop(void){
-	HAL_GPIO_TogglePin(LED);
+	buttons = 0;
+
+	if(HAL_GPIO_ReadPin(BUT_UP))
+		buttons++;
+	if(HAL_GPIO_ReadPin(BUT_DWN))
+		buttons++;
+	if(HAL_GPIO_ReadPin(BUT_LFT))
+		buttons++;
+	if(HAL_GPIO_ReadPin(BUT_RGT))
+		buttons++;
+
+	if(buttons == 1){
+		if(HAL_GPIO_ReadPin(BUT_UP))
+			lcd_string("UP             ", 0, 0);
+		if(HAL_GPIO_ReadPin(BUT_DWN))
+			lcd_string("DOWN           ", 0, 0);
+		if(HAL_GPIO_ReadPin(BUT_LFT))
+			lcd_string("LEFT           ", 0, 0);
+		if(HAL_GPIO_ReadPin(BUT_RGT))
+			lcd_string("RIGHT          ", 0, 0);
+	}
+	else
+		if(buttons == 0)
+			lcd_string("me aperta vai", 0, 0);
+		else
+			lcd_string("para com isso", 0, 0);
+
+
 	HAL_Delay(100);
 }
+
+
+
+
+
--- FinalProject/README.md
+++ FinalProject/README.md
@@ -4,11 +4,10 @@ This is the folder of the final project! It consists of a prototype of my bachel
 
 TODO LIST:
 - [x] blink a LED to test the HAL
-- [ ] Implement the LCD interface
-- [ ] Implement the buttons functionalies
+- [x] Implement the LCD interface
+- [x] Implement the buttons functionalies
 - [ ] Implement the soft-potentiometer functionalies
-- [ ] Implement the state machine and scheduler structures
-- [ ] Implement the DAC functionalities
-- [ ] Implement the state machine and scheduler
+- [ ] Implement the UI state machine and structures
+- [ ] Implement the DAC functionalities with interruptions
+- [ ] Implement the mini DDS engine
 - [ ] test
-- [ ] UI and menus
